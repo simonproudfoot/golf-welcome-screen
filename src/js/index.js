00444e6dea1ac510815902273376d3fea52763ed
simonproudfoot <simonproudfoot1980@gmail.com>
2021-05-06 15:59:47 +0100

    Transitions working

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import '../scss/style.scss'
 import Progress from 'vue-multiple-progress'
 import gsap from "gsap";
+
 Vue.component('vm-progress', Progress)
 Vue.use(Progress)
 
@@ -15,7 +16,8 @@ var app = new Vue({
       countDown: 5,
       duration: 5,
       sequence: 3,
-      elements: gsap.utils.toArray(".content")
+      elements: gsap.utils.toArray(".content"),
+      firstTime: true
     }
   },
   methods: {
@@ -39,27 +41,32 @@ var app = new Vue({
       var content = '.content-' + this.sequence
       var image = '.backImage-' + this.sequence
       gsap.fromTo(content, { x: -100, opacity: 0 }, { x: 0, opacity: 1, duration: 1 })
-      gsap.fromTo(image, { scale: 1.1, opacity: 0 }, { scale: 1.0, opacity: 1, duration: 1})
-    
+      gsap.fromTo(image, { scale: 1.2, opacity: 0 }, { scale: 1.1, opacity: 1, duration: 1 })
       gsap.to(content, { x: -100, opacity: 0, duration: 0.5, delay: this.duration })
-      gsap.to(image, { scale: 0.9, opacity: 0, duration: 0.5, delay: this.duration })
+      gsap.to(image, { scale: 1, opacity: 0, duration: 0.5, delay: this.duration })
 
     },
     runAnim() {
-      // only works for 2
-      // this.tl.fromTo('.content', { opacity: 0, x: -30 }, { opacity: 1, x: 0, stagger: this.duration }, "-=0.4").to('.content', { opacity: 0, x: -10, stagger: this.duration }, '=-1')
-      // this.tlImages.fromTo('.backImage', { opacity: 0, scale: 1.2 }, { opacity: 1, scale: 1.1, stagger: this.duration }, "-=0.4").to('.backImage', { opacity: 0, scale: 1, stagger: this.duration }, '=-1')
-      if (this.sequence > 0) {
-        setTimeout(() => {
-          this.sequence -= 1
-          this.animationA()
+
+      if (!this.firstTime) {
+        if (this.sequence > 0) {
+          setTimeout(() => {
+            this.sequence -= 1
+            this.animationA()
+            this.runAnim()
+          }, this.duration * 1000)
+        } else {
+          this.sequence = Object.keys(this.elements).length
           this.runAnim()
-        }, this.duration * 1000)
+        }
       } else {
-        this.sequence = Object.keys(this.elements).length
-        this.runAnim()
+        this.sequence = 0
+        this.animationA()
+        this.firstTime = false
+        setTimeout(() => {
+          this.runAnim()
+        }, this.duration)
       }
-      console.log(this.sequence)
     }
   },
   computed: {
@@ -73,8 +80,8 @@ var app = new Vue({
   },
   mounted() {
     // console.log(Object.keys(this.elements).length)
-    gsap.set('.content', { opacity: 0, x: -100 })
-    gsap.set('.backImage', { opacity: 0, scale: 1.1 })
+
+
     this.runAnim()
     this.timer()
   }
